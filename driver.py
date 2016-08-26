45559aaa50d750088aa721de4b90fbe56e848cba
JP Quicksall <john.paul.quicksall@gmail.com>
2016-08-26 14:54:21 -0700

    Finalized calculations and response functions

--- driver.py
+++ driver.py
@@ -1,11 +1,18 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler import events
+from flask import Flask
+from datetime import datetime
 import methods
 import os
-from datetime import datetime
 import time
 
+app = Flask(__name__)
 scheduler = BackgroundScheduler()
+port = int(os.getenv('VCAP_APP_PORT', 8080))
+
+@app.route('/')
+def index_route():
+  return 'Welcome to the labmunger api'
 
 if __name__ == '__main__':
   scheduler.add_job(methods.get_message_from_SQS, 'interval', seconds = 3)
@@ -13,8 +20,8 @@ if __name__ == '__main__':
   scheduler.start()
 
   try:
-    while True:
-      time.sleep(2)
+    app.run(host='0.0.0.0', port=port)
+
   except (KeyboardInterrupt, SystemExit):
     # Not strictly necessary if daemonic mode is enabled but should be done if possible
     scheduler.shutdown()
--- methods.py
+++ methods.py
@@ -4,6 +4,8 @@ import json
 import requests
 import os
 
+
+lab_url = "http://labapi.bellevuelab.isus.emc.com/"
 # Resources:
 # - http://boto3.readthedocs.io/en/latest/guide/resources.html
 def get_message_from_SQS():
@@ -13,20 +15,32 @@ def get_message_from_SQS():
   # Get the queue
   queue = sqs.get_queue_by_name(QueueName='lab_comm')
 
+  messages = queue.receive_messages(MessageAttributeNames=['AmazonIntent'])
   # Process messages by printing out body and optional author name
-  for message in queue.receive_messages(MessageAttributeNames=['AmazonIntent']):
+  for message in messages:
     print("We got a message")
     # Get the custom author message attribute if it was set
     eventType = ""
+    print (message)
     if message.message_attributes is not None:
       eventType = message.message_attributes.get('AmazonIntent').get('StringValue')
       print("The message was from Alexa and the intent is {0}".format(eventType))
       # event = json.loads(message.body)
       if eventType == "AllVMsCount":
         print('Need to count all of the vms')
+        vm_count = fetch_vm_count()
+        print("There are {0} vms in the lab".format(vm_count))
+        response = sqs.get_queue_by_name(QueueName='lab_comm_2').send_message(MessageBody="There are {0} vms in the lab.".format(vm_count), MessageAttributes={"AmazonResponse":{"StringValue":eventType,'DataType': 'String'}})
+        print(response)
+        # Let the queue know that the message is processed
+        message.delete()
 
-    # Let the queue know that the message is processed
-    message.delete()
+def fetch_vm_count():
+  vm_count = 0
+  r = requests.get('{0}vms/'.format(lab_url))
+  vms = r.json()['vm']
+  vm_count = len(vms)
+  return vm_count
 
 # listener to give visibility into job completetion
 def error_listener(event):
